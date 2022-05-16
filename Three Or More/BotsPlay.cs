026bb763a543d990c3c9612fe63862ec276d4898
Nathan Mace <32456247+NathanMace7@users.noreply.github.com>
2022-05-16 13:56:10 +0100

    Added Scoring mechanic
    Added and fixed a mechanic where the player and bot gets points, and if either get 50 or above then the game ends, prompting the user to play again or to exit

--- Three Or More/BotsPlay.cs
+++ Three Or More/BotsPlay.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Three_Or_More.Program;
 using static Three_Or_More.PlayersPlay;
 
 namespace Three_Or_More
 {
     class BotsPlay
     {
-        public static void BotPlay()
+        public static void BotPlay(int playerscore, int botscore)
         {
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
@@ -20,65 +21,65 @@ namespace Three_Or_More
             int bpdice3 = rnd.Next(1, 7);  //Chooses the bot's dice 1 value
             int bpdice4 = rnd.Next(1, 7);  //Chooses the bot's dice 1 value
             int bpdice5 = rnd.Next(1, 7);  //Chooses the bot's dice 1 value
-            int score = 0;
+            
 
             //If all 5 dice are the same
             if (bpdice1 == bpdice2 && bpdice2 == bpdice3 && bpdice3 == bpdice4 && bpdice4 == bpdice5)
-            { Console.WriteLine("The bot's dice all dice are the same."); score = score + 12; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot's dice all dice are the same."); botscore = botscore + 12; Console.WriteLine("Bot's score is: " + botscore); }
 
             //If The bot rolled the same 4 dice
             else if (bpdice1 == bpdice2 && bpdice2 == bpdice3 && bpdice3 == bpdice4)    //If dice 1, 2, 3 and 4 are the same
-            { Console.WriteLine("The bot rolled the same 4 dice."); score = score + 6; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 4 dice."); botscore = botscore + 6; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice2 && bpdice2 == bpdice3 && bpdice3 == bpdice5)    //If dice 1, 2, 3 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 4 dice."); score = score + 6; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 4 dice."); botscore = botscore + 6; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice2 && bpdice2 == bpdice4 && bpdice4 == bpdice5)    //If dice 1, 2, 4 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 4 dice."); score = score + 6; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 4 dice."); botscore = botscore + 6; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice3 && bpdice3 == bpdice4 && bpdice4 == bpdice5)    //If dice 1, 3, 4 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 4 dice."); score = score + 6; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 4 dice."); botscore = botscore + 6; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice2 == bpdice3 && bpdice3 == bpdice4 && bpdice4 == bpdice5)    //If dice 2, 3, 4 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 4 dice."); score = score + 6; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 4 dice."); botscore = botscore + 6; Console.WriteLine("Bot's score is: " + botscore); }
 
             //If the bot rolled the same 3 dice
             else if (bpdice1 == bpdice2 && bpdice2 == bpdice3)  //If dice 1, 2 and 3 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice2 && bpdice2 == bpdice4)  //If dice 1, 2 and 4 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice2 && bpdice2 == bpdice5)  //If dice 1, 2 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice3 && bpdice3 == bpdice4)  //If dice 1, 3 and 4 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice3 && bpdice3 == bpdice5)  //If dice 1, 3 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice1 == bpdice4 && bpdice4 == bpdice5)  //If dice 1, 4 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice2 == bpdice3 && bpdice3 == bpdice4)  //If dice 2, 3 and 4 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice2 == bpdice3 && bpdice3 == bpdice5)  //If dice 2, 3 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice2 == bpdice4 && bpdice4 == bpdice5)  //If dice 2, 4 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
             else if (bpdice3 == bpdice4 && bpdice4 == bpdice5)  //If dice 3, 4 and 5 are the same
-            { Console.WriteLine("The bot rolled the same 3 dice."); score = score + 3; Console.WriteLine("Bot's score is: " + score); }
+            { Console.WriteLine("The bot rolled the same 3 dice."); botscore = botscore + 3; Console.WriteLine("Bot's score is: " + botscore); }
 
             //If the bot rolled the same 2 dice
-            else if (bpdice1 == bpdice2) { Console.WriteLine("Dice 1 and 2 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice1 == bpdice3) { Console.WriteLine("Dice 1 and 3 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice1 == bpdice4) { Console.WriteLine("Dice 1 and 4 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice1 == bpdice5) { Console.WriteLine("Dice 1 and 5 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice2 == bpdice3) { Console.WriteLine("Dice 2 and 3 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice2 == bpdice4) { Console.WriteLine("Dice 2 and 4 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice2 == bpdice5) { Console.WriteLine("Dice 2 and 5 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice3 == bpdice4) { Console.WriteLine("Dice 3 and 4 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice3 == bpdice5) { Console.WriteLine("Dice 3 and 5 are the same. The bot gets to re-roll!"); BotReroll(); }
-            else if (bpdice4 == bpdice5) { Console.WriteLine("Dice 4 and 5 are the same. The bot gets to re-roll!"); BotReroll(); }
+            else if (bpdice1 == bpdice2) { Console.WriteLine("Dice 1 and 2 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice1 == bpdice3) { Console.WriteLine("Dice 1 and 3 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice1 == bpdice4) { Console.WriteLine("Dice 1 and 4 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice1 == bpdice5) { Console.WriteLine("Dice 1 and 5 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice2 == bpdice3) { Console.WriteLine("Dice 2 and 3 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice2 == bpdice4) { Console.WriteLine("Dice 2 and 4 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice2 == bpdice5) { Console.WriteLine("Dice 2 and 5 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice3 == bpdice4) { Console.WriteLine("Dice 3 and 4 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice3 == bpdice5) { Console.WriteLine("Dice 3 and 5 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
+            else if (bpdice4 == bpdice5) { Console.WriteLine("Dice 4 and 5 are the same. The bot gets to re-roll!"); BotReroll(playerscore, botscore); }
 
             //Else, if no dice are the same
             else { Console.WriteLine("No two die are the same. What a shame! Next, the player's turn!"); }
             Console.ResetColor();
-            Player();
+            Player(playerscore, botscore); //Goes to the player
         }
 
-        public static void BotReroll()
+        public static void BotReroll(int playerscore, int botscore)
         {
             //This method is for when the User gets to re-roll because two of the previous dice were the same value
             //Console.WriteLine("\nDice rolls: \n");  //Tells user the rolled the dice
@@ -86,26 +87,26 @@ namespace Three_Or_More
             int bpdice1 = rnd.Next(1, 7); //Chooses dice 1's value
             int bpdice2 = rnd.Next(1, 7); //Chooses dice 2's value
             int bpdice3 = rnd.Next(1, 7); //Chooses dice 3's value
-            int score = 0;  //Sets score
+
             //Displays dice rolls ↓
             //Console.WriteLine("You rolled a " + bpdice1 + ".\nYou rolled a " + bpdice2 + ".\nYou rolled a " + bpdice3 + ".\n");
 
             //If all other 3 dice are the same
             if (bpdice1 == bpdice2 && bpdice2 == bpdice3)
-            { Console.WriteLine("All 3 other dice are the same."); score = score + 12; Console.WriteLine("The bot's score is: " + score); }
+            { Console.WriteLine("All 3 other dice are the same."); botscore = botscore + 12; Console.WriteLine("The bot's score is: " + botscore); }
 
             //If 2 other dice are the same
             else if (bpdice1 == bpdice2)    //If dice 1 and 2 are the same
-            { Console.WriteLine("2 other dice are the same."); score = score + 6; Console.WriteLine("The bot's score is: " + score); }
+            { Console.WriteLine("2 other dice are the same."); botscore = botscore + 6; Console.WriteLine("The bot's score is: " + botscore); }
             else if (bpdice1 == bpdice3)    //If dice 1 and 3 are the same
-            { Console.WriteLine("2 other dice are the same."); score = score + 6; Console.WriteLine("The bot's score is: " + score); }
+            { Console.WriteLine("2 other dice are the same."); botscore = botscore + 6; Console.WriteLine("The bot's score is: " + botscore); }
             else if (bpdice2 == bpdice3)    //If dice 2 and 3 are the same
-            { Console.WriteLine("2 other dice are the same."); score = score + 6; Console.WriteLine("The bot's score is: " + score); }
+            { Console.WriteLine("2 other dice are the same."); botscore = botscore + 6; Console.WriteLine("The bot's score is: " + botscore); }
 
             //Else, if no other dice are the same
             else { Console.WriteLine("Unfortunatly no other dice are the same. What a shame! Next, the player's turn!"); }
             Console.ResetColor();
-            Player(); //Goes to the player
+            Score(playerscore, botscore);
         }
     }
 }
--- Three Or More/Program.cs
+++ Three Or More/Program.cs
@@ -6,7 +6,7 @@ namespace Three_Or_More
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("Welcome to Three Or More\n");    //Title
             Console.WriteLine("Rules of Three Or More: \n" +    //Rules
@@ -14,9 +14,76 @@ namespace Three_Or_More
                 "\nIf a player only has two-of-a-kind, they may re-throw the remaining dice\nin an attempt to improve the remaining dice values." +
                 "\nIf no matching numbers are rolled after two rolls, the player scores 0." +
                 "\n\nIn this game, you will be up against a bot.\nThe bot does not tell you its rolls (mainly for simplicity on the console), but will give you its scoring and tell you if it needs a re-roll.");
-            Player();
+            ScoreSet();
         }
-        
-        
+
+        public static void ScoreSet() 
+        { 
+            int playerscore = 0; 
+            int botscore = 0;
+            Score(playerscore, botscore);
+        }
+        public static void Score(int playerscore, int botscore)
+        {
+            if (playerscore >= 50) 
+            { 
+                Console.WriteLine("\nAnd the Player wins!\nPlayer's score: " + playerscore + "\nThe bot's score: " + botscore);
+                Console.WriteLine("\nWould you like to play again?");  //Program promts user to play again
+                bool continuepar = false;   //Bool used for continuing the program after a choice is made.
+                do
+                {
+                    string chooseToContinue = Console.ReadLine();
+                    switch (chooseToContinue)
+                    {
+                        case "1": Main(); break;    //User can input '1' for 'Yes'
+                        case "roll": Main(); break; //User can input 'roll'...
+                        case "Roll": Main(); break; //...or 'Roll' to choose to continue
+                        case "yes": Main(); break;  //User can input 'yes'... 
+                        case "Yes": Main(); break;  //...or 'Yes' to choose to continue
+                        case "y": Main(); break;    //User can input 'y'... 
+                        case "Y": Main(); break;    //...or 'Y' to choose to continue
+                        case "0": Console.WriteLine("Thanks for playing! Please press any putton to quit"); Console.ReadKey(); Environment.Exit(0); break;  //User can input '0' for 'No'
+                        case "no": Console.WriteLine("Thanks for playing! Please press any putton to quit"); Console.ReadKey(); Environment.Exit(0); break; //User can input 'no'... 
+                        case "No": Console.WriteLine("Thanks for playing! Please press any putton to quit"); Console.ReadKey(); Environment.Exit(0); break; //...or 'No' to choose to quit
+                        case "n": Console.WriteLine("Thanks for playing! Please press any putton to quit"); Console.ReadKey(); Environment.Exit(0); break;  //User can input 'n'... 
+                        case "N": Console.WriteLine("Thanks for playing! Please press any putton to quit"); Console.ReadKey(); Environment.Exit(0); break;  //...or 'N' to choose to quit
+                        default:
+                            Console.WriteLine("Valid responses for yes: '1', 'Roll', 'Yes' or 'Y'\nValid responses for no: '0', 'No', or 'N'"); //If the input is invalid, print this line
+                            break;  //Stops when the program is finished
+                    }
+                }
+                while (continuepar == false);   //Tells the program to repeat the question
+            }
+            else if (botscore >= 50) 
+            { 
+                Console.WriteLine("And the Bot wins! \nThe bot's score: " + botscore + "\nPlayer's score: " + playerscore);
+                Console.WriteLine("\nWould you like to play again?");  //Program promts user to play again
+                bool continuepar = false;   //Bool used for continuing the program after a choice is made.
+                do
+                {
+                    string chooseToContinue = Console.ReadLine();
+                    switch (chooseToContinue)
+                    {
+                        case "1": Main(); break;    //User can input '1' for 'Yes'
+                        case "roll": Main(); break; //User can input 'roll'...
+                        case "Roll": Main(); break; //...or 'Roll' to choose to continue
+                        case "yes": Main(); break;  //User can input 'yes'... 
+                        case "Yes": Main(); break;  //...or 'Yes' to choose to continue
+                        case "y": Main(); break;    //User can input 'y'... 
+                        case "Y": Main(); break;    //...or 'Y' to choose to continue
+                        case "0": Main(); break;    //User can input '0' for 'No'
+                        case "no": Main(); break;   //User can input 'no'... 
+                        case "No": Main(); break;   //...or 'No' to choose to continue
+                        case "n": Main(); break;    //User can input 'n'... 
+                        case "N": Main(); break;    //...or 'N' to choose to continue
+                        default:
+                            Console.WriteLine("Valid responses for yes: '1', 'Roll', 'Yes' or 'Y'\nValid responses for no: '0', 'No', or 'N'"); //If the input is invalid, print this line
+                            break;  //Stops when the program is finished
+                    }
+                }
+                while (continuepar == false);   //Tells the program to repeat the question
+            }
+            else { Player(playerscore, botscore); }
+        }
     }
 }
